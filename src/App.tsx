31ac1d303c348fa8905d5bbefe19e0fb9ef0e100
Humberto Henrique Campos Pinheiro <humbhenri@gmail.com>
2026-07-31 16:04:38 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles/theme.css";
+import "./styles/global.css";
+import Heading from "./components/Heading";
+
+export default function App() {
+  return (
+    <>
+      <Heading>Olá mundo</Heading>
+    </>
+  );
+}
