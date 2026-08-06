3d9db328a470708d8d4ffa9e5eeb7e38459e80cd
Humberto Henrique Campos Pinheiro <humberto.pinheiro@serpro.gov.br>
2026-08-06 16:01:00 -0300

    add light/dark theme support

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,20 +1,39 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
+import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon, MoonIcon } from "lucide-react";
 import styles from "./styles.module.css";
+import { useState, useEffect } from 'react';
 
-export function Menu() {  
+type Theme = 'dark' | 'light';
+
+export function Menu() {
+
+  const [theme, setTheme] = useState<Theme>(() => {
+    const storageTheme = localStorage.getItem('theme') as Theme || 'dark';
+    return storageTheme;
+  });
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  const handleThemeChange = (event: React.MouseEvent) => {
+    event.preventDefault();
+    setTheme(theme == 'dark' ? 'light' : 'dark');
+  };
+
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href="#">
+      <a className={styles.menuLink} href="#" aria-label="Home" title="Home">
         <HouseIcon />
       </a>
-      <a className={styles.menuLink} href="#">
+      <a className={styles.menuLink} href="#" aria-label="Histórico" title="Histórico">
         <HistoryIcon />
       </a>
-      <a className={styles.menuLink} href="#">
+      <a className={styles.menuLink} href="#" aria-label="Configurações" title="Configurações">
         <SettingsIcon />
       </a>
-      <a className={styles.menuLink} href="#">
-        <SunIcon />
+      <a className={styles.menuLink} href="#" aria-label="Mudar tema" title="Mudar tema" onClick={handleThemeChange}>
+        {theme == 'dark' ? <SunIcon /> : <MoonIcon />}
       </a>
     </nav>
   );
