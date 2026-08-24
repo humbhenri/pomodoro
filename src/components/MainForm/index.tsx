67477bc4b2e741f11958793ece8cc9ba8c487d14
Humberto Henrique Campos Pinheiro <humbhenri@gmail.com>
2026-08-24 20:23:56 -0300

    add last task name in input

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -14,6 +14,7 @@ import { dismiss, showMessage } from "../../adapters/showMessage";
 export function MainForm() {
   const taskNameInput = useRef<HTMLInputElement>(null);
   const { state, dispatch } = useTaskContext();
+  const lastTaskName = state.tasks.at(-1)?.name || "";
   const numero = useRef<number>(0);
 
   // ciclos
@@ -68,6 +69,7 @@ export function MainForm() {
           placeholder="Name your task"
           ref={taskNameInput}
           disabled={!!state.activeTask}
+          defaultValue={lastTaskName}
         />
       </div>
       <div className="formRow">
